a304778dde8ae6e7e073c4de3a80ae20c22ac32e
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-01-13 22:32:19 +0300

    Initialize

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.AddControllers{
+
+  [ApiController]
+  [Route("[controller]s")]
+  public class BookController : ControllerBase
+  {
+    
+
+  }
+}
