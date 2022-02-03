869c907c5a34699b6f5edf7cf7a59cf55f1a7fc6
oguzdemir <oguz.demir@mirsis.com.tr>
2022-02-03 22:53:21 +0300

    Author|1 Initial Author

--- WebApi/Entities/Book.cs
+++ WebApi/Entities/Book.cs
@@ -8,11 +8,12 @@ namespace WebApi.Entities
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Title { get; set; }
-    public int GenreId { get; set; }
-
-    // Give Foreign key 
-    public Genre Genre {get; set;}
     public int PageCount { get; set; }
-    public DateTime PublishDate { get; set; }  
+    public DateTime PublishDate { get; set; }      
+    public int GenreId { get; set; }
+    public int AuthorId { get; set; } 
+
+    public Genre Genre {get; set;}    
+    public Author Author { get; set; }
   }
 }
